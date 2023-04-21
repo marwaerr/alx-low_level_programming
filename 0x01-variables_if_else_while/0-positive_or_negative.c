0934792f1fa7d1152f9cea979306f270aa5f8de7
marwaerr <marwaerramla@gmail.com>
2023-04-21 16:46:12 -0700

    herer is the name

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,16 @@
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
 
+
 /**
- * main - Entry point
+ * main - Prints if number is positive, zero or negative
+ *Return: Always  (Success)
  *
- * Return: Always 0 (Success)
  */
+
 int main(void)
 {
         int n;
