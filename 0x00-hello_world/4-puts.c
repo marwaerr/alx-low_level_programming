01654d8c764bd1462636731a674bfc7a513b1e6c
marwaerr <marwaerramla@gmail.com>
2023-04-20 19:38:08 -0700

    here is my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,11 @@
 /**
  * main - Entry point
  *
- * Description: Prints the exact string "Programming is like building a
- * multilingual puzzle", followed by a new line, using the puts function.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
 
