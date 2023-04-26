cbc6eeca3e5f01bcc469328cea3a0060d521acb8
marwaerr <marwaerramla@gmail.com>
2023-04-25 17:05:36 -0700

    hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,21 @@
-#include "_putchar.h"
+#include "main.h"
+/**
+ *main-entry point
+ description prints putchar to student
+ return always 0
+ */
 
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return (0);
+       	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
 
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,4 @@
 here is my 
+[master 7e7851e] ddf
+ 1 file changed, 1 insertion(+)
+ create mode 100644 0x02-functions_nested_loops/README.md
