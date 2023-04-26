21ad859d1151cc6183eaf5db79767c42c9bc2cf5
marwaerr <marwaerramla@gmail.com>
2023-04-25 18:26:08 -0700

    hgfd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,11 +8,12 @@
  * Return: 1 for lowercase character. O for the rest.
  */
 int _islower(int c)
-if (с >= 97 && с <= 122)
+{
+	if (с >= 97 && с <= 122)
 {
 	return (1);
 }
-else
+	else
 {
 	return (a);
 }
